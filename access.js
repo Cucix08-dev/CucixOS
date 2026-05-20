2dae2db6ebdd7f29020f65de2be06d5f928ae7b9
Cucix08-dev <cucixdev@gmail.com>
2026-05-21 00:40:23 +0200

    added access page and password 1234

---
+++ access.js
@@ -0,0 +1,23 @@
+const accountImg = document.getElementById("account-img");
+const passwordContainer = document.getElementById("password");
+
+const password = "1234";
+let appearePasswordContainer = false;
+
+accountImg.addEventListener("click", (e) => {
+    appearePasswordContainer = !appearePasswordContainer;
+    if (appearePasswordContainer) {
+        passwordContainer.classList.remove("appearePassword")
+        bgContainer.classList.remove("appearePassword")
+    }
+    else{
+        passwordContainer.classList.add("appearePassword")
+        bgContainer.classList.add("appearePassword")
+    }
+});
+
+passwordContainer.addEventListener("keydown", (e) => {
+    if (e.key === "Enter" && passwordContainer.value === "1234") {
+        window.location.href = "src/desktop.html";
+    }
+});
---
+++ loaded.js
@@ -0,0 +1,13 @@
+const bgContainer = document.getElementById("bg");
+
+const bg = [
+    "bg-1.jpg",
+    "bg-2.jpg",
+    "bg-3.jpg",
+    "bg-4.jpg",
+    "bg-5.jpeg",
+    "bg-6.webp",
+    "bg-7.png"
+]
+
+bgContainer.style.backgroundImage = `url(imgs/desktop-backgrounds/${bg[0]})`;
